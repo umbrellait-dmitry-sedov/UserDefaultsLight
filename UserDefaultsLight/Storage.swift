178eb2a1a63ed9e0068aa20a089c0cff0680d3aa
umbrellait <dmitry.sedov@umbrellait.com>
2020-11-09 15:11:26 +0300

    Initial Commit

---
+++ UserDefaultsLight/Storage.swift
@@ -0,0 +1,34 @@
+//
+//  UDManager.swift
+//  StorageUDExamples
+//
+//  Created by Dmitry Sedov on 06.11.2020.
+//
+
+import Foundation
+
+public class Storage {
+    
+    private let defaults = UserDefaults.standard
+    
+    static let shared = Storage()
+    
+    public func setValue<T>(object: T, key: String) { // hide impementtion archiving
+        if let savedData = try? NSKeyedArchiver.archivedData(withRootObject: object, requiringSecureCoding: false) {
+            print("value was added to key")
+            defaults.set(savedData, forKey: key)
+        } else {
+            defaults.removeObject(forKey: key)
+        }
+    }
+    
+    public func getValue<T>(model: T, key: String) -> T? { // hide impementtion archiving
+        guard let savedData = UserDefaults.standard.object(forKey: key) as? Data, let decodedModel = try? NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(savedData) as? T else { return nil }
+        return decodedModel
+    }
+    
+    public func objectIsExist(forKey key: String) -> Bool {
+        let flag = defaults.object(forKey: key) != nil ? true : false // Chech the value for nil
+        return flag
+      }
+}
---
+++ UserDefaultsLight/UserDefaultsLight.h
@@ -0,0 +1,18 @@
+//
+//  UserDefaultsLight.h
+//  UserDefaultsLight
+//
+//  Created by Dmitry Sedov on 09.11.2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UserDefaultsLight.
+FOUNDATION_EXPORT double UserDefaultsLightVersionNumber;
+
+//! Project version string for UserDefaultsLight.
+FOUNDATION_EXPORT const unsigned char UserDefaultsLightVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UserDefaultsLight/PublicHeader.h>
+
+
