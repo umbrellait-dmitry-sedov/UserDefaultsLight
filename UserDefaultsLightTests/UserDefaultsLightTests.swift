ae673363a3fda5eb8c08f43a897e6677a0f2e600
Rezvantsev Viktor <viktor.rezvantsev@umbrellait.com>
2020-11-11 15:17:40 +0300

    delete empty line and test all public functions

--- UserDefaultsLightTests/UserDefaultsLightTests.swift
+++ UserDefaultsLightTests/UserDefaultsLightTests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+@testable import UserDefaultsLight
 
 class UserDefaultsLightTests: XCTestCase {
 
@@ -28,5 +29,85 @@ class UserDefaultsLightTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
+
+}
+
+struct UserMock: Codable {
+    let name: String?
+    let url: String?
+    let age: Int?
+    let pictureUrl: String?
+    
+    init(name: String?, url: String?, age: Int?, pictureUrl: String?) {
+        self.name = name
+        self.url = url
+        self.age = age
+        self.pictureUrl = pictureUrl
+    }
+}
+
+class UserStorageTests: XCTestCase {
+    
+    // MARK: - Properties
+    
+    var user = UserMock(name: "Peter",
+                         url: "www.taganrog.com",
+                         age: 45,
+                         pictureUrl: "myprofile/img45.jpg")
+    let user2 = UserMock(name: "John",
+                          url: "www.ru-ru",
+                          age: 25,
+                          pictureUrl: "myprofile/img01.jpg")
+    let key = "InfiniteKey"
+    
+    // MARK: - Helpers
+    
+    func makeSUT() -> Storage {
+        return Storage.shared
+    }
+    
+    // MARK: - Test functions
+    
+    func testValidKey() {
+        XCTAssertFalse(Storage.shared.isValueExists(forKey: "user"))
+    }
+    
+    func testSetValue() {
+        makeSUT().setValue(user, forKey: key)
+        guard let object: UserMock = makeSUT().getValue(forKey: key) else {
+            return
+        }
+        XCTAssertNotNil(object)
+    }
+    
+    func testGetValue() {
+        guard let object: UserMock = makeSUT().getValue(forKey: key) else {
+            return
+        }
+        XCTAssertNil(object)
+    }
+    
+    func testUpdateValue() {
+        makeSUT().setValue(user, forKey: key)
+        guard let object1: UserMock = makeSUT().getValue(forKey: key) else {
+            return
+        }
+        makeSUT().updateValue(object: user2, forKey: key)
+        guard let object2: UserMock = makeSUT().getValue(forKey: key) else {
+            return
+        }
+        XCTAssertNotEqual(object1.name, object2.name)
+    }
+    
+    func testDeleteObject() {
+        makeSUT().setValue(user, forKey: key)
+        makeSUT().removeValue(forKey: key)
+        guard let object: UserMock = makeSUT().getValue(forKey: key) else {
+            return
+        }
+        XCTAssertNil(object)
+    }
+    
+    
 
 }
